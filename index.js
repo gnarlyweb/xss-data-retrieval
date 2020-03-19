7089e1420244fc89815f936a83be15a87a9fb38c
freitzzz <1160907@isep.ipp.pt>
2020-03-19 20:57:46 +0000

    More info on request

--- index.js
+++ index.js
@@ -14,16 +14,11 @@ const app = express();
 
 app.get('/', async (request, response, next) => {
   let data = request.query.data;
+  let headers = request.headers;
   await PayloadCollection.create({ data: data });
   console.log(`data => ${data}`);
+  console.log(`headers => ${headers}`);
   response.status(204).send();
-});
-
-app.options('/', function (req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Methods', '*');
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  res.end();
 });
 
 app.listen(process.env.PORT);

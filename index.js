5f1460fc625dda4fc9b247477390fef1382da657
freitzzz <1160907@isep.ipp.pt>
2020-03-19 19:54:46 +0000

    Will it work now

--- index.js
+++ index.js
@@ -19,11 +19,13 @@ app.get('/', async (request, response, next) => {
   response.status(204).send();
 });
 
-app.all('/*', function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
+app.options('/', function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.end();
 });
+
 app.listen(process.env.PORT);
 
 moongoose.connect(process.env.MONGODB_CONNECTION_URL, { useNewUrlParser: true });

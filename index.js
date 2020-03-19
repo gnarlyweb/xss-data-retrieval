4339d4525437550a717d78285cdfafed99e59103
freitzzz <1160907@isep.ipp.pt>
2020-03-19 19:23:32 +0000

    Added mongodb communication

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 
+const moongoose = require('mongoose');
+
+const Payload = moongoose.Schema({
+  data: String
+});
+
+const PayloadCollection = moongoose.model('Payload', Payload);
+
 const app = express();
 
-app.get('/', (request, response, next) => {
+app.get('/', async (request, response, next) => {
   let data = request.query.data;
+  await PayloadCollection.create({ data: data });
   console.log(`data => ${data}`);
-  response.status(200);
+  response.status(204).send();
 });
 
 app.listen(8080);
+
+moongoose.connect(process.env.MONGODB_CONNECTION_URL, { useNewUrlParser: true });
